1bdd07f66faa6fcef0c1a6b7f379c6fc51d7df0e
Nathan Daniel Peterson <natperson@gmail.com>
2018-02-08 06:21:34 -0800

    practice with python data structures

---
+++ lists_tuples_sets.py
@@ -0,0 +1,17 @@
+
+grades = [71, 78, 83, 85, 90]
+tuple_grades = ( 72, 79, 80, 86)
+set_grades = { 83, 75, 102, 96}
+
+# print(sum(grades) / len(grades))
+
+# grades.append(100)
+# print(grades)
+# set_grades.add(89)
+
+your_lottery_nums = { 1, 2, 3, 4, 5}
+# winning_lotter_nums = { 1, 3, 5, 7, 13, 117}
+
+# print(your_lottery_nums.intersection(winning_lotter_nums))
+your_lottery_nums.add(6)
+print(your_lottery_nums)
---
+++ variables_methods.py
@@ -0,0 +1,13 @@
+a = 5
+# print(a)
+
+
+def my_print_method(arg):
+    print(arg)
+
+
+def multiply_method(num1, num2):
+    return num1 * num2
+
+result = multiply_method(5,3)
+my_print_method(result)
